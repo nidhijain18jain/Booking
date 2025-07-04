1e1bccb9974a03eb3b4e456a5c6b18dd7919d8b8
nidhijain18jain <nidhijain181993@gmail.com>
2025-07-04 11:14:32 +0530

    Update HotelService.java

--- src/main/java/com/booking/recruitment/hotel/service/HotelService.java
+++ src/main/java/com/booking/recruitment/hotel/service/HotelService.java
@@ -3,11 +3,18 @@ package com.booking.recruitment.hotel.service;
 import com.booking.recruitment.hotel.model.Hotel;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HotelService {
   List<Hotel> getAllHotels();
 
+  Optional<Hotel> getHotelById(Long id);
+
   List<Hotel> getHotelsByCity(Long cityId);
 
   Hotel createNewHotel(Hotel hotel);
+
+  Hotel deleteHotelById(Long Id);
+
+  List<Hotel> findHotelsClosestToCityCenter(Long cityId);
 }
