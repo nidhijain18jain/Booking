d668ce8a890465c438fc008c3b6270230b9b11dd
nidhijain18jain <nidhijain181993@gmail.com>
2025-07-04 11:12:50 +0530

    Update HotelRepository.java

--- src/main/java/com/booking/recruitment/hotel/repository/HotelRepository.java
+++ src/main/java/com/booking/recruitment/hotel/repository/HotelRepository.java
@@ -1,6 +1,21 @@
 package com.booking.recruitment.hotel.repository;
 
 import com.booking.recruitment.hotel.model.Hotel;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface HotelRepository extends JpaRepository<Hotel, Long> {}
+import java.util.List;
+
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+    String HAVERSINE_FORMULA = "(6371 * acos(cos(radians(:latitude)) * cos(radians(h.latitude)) *" +
+            " cos(radians(h.longitude) - radians(:longitude)) + sin(radians(:latitude)) * sin(radians(h.latitude))))";
+
+    @Query("SELECT h FROM Hotel h WHERE h.city.id = :cityId ORDER BY "+ HAVERSINE_FORMULA + " ASC")
+    List<Hotel> findHotelsClosestToCityCenter(
+            @Param("latitude") double latitude,
+            @Param("longitude") double longitude,
+            @Param("cityId") Long cityId,
+            Pageable pageable);
+}
